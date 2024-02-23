a486813c983d5c28be2d5e3b60b444132e7c1e1e
Hannah <github@rennersh.eu>
2024-02-23 11:38:20 +0100

    Docker update
    Now uses Ubuntu instead of Python, installs packages manually, also installs nodejs

---
+++ backend/codeapi/run-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# For testing purposes
+# you can append "python3", "nodejs", "bash", "fish" or any other command to the end of the docker run command
+
+docker run -it -w "/home/codeapi" --rm --name codeapi codeapi
