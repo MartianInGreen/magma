fe7c378743b7af353ebc0046f34e8f4da9e4d6ab
Hannah <github@rennersh.eu>
2024-02-24 21:09:24 +0100

    Basic user creation & login

--- src/api/users.py
+++ src/api/users.py
@@ -2,3 +2,87 @@
 ### File: users.py
 ### Authors: Hannah Renners
 ### ----------------------------------------------------
+
+# ----------------------------------------------------
+# Imports
+# ----------------------------------------------------
+
+import tinydb
+import uuid
+
+# ----------------------------------------------------
+# User Management
+# ----------------------------------------------------
+
+def createUser(userEmail: str, userName: str, userDisplayName: str):
+    db = tinydb.TinyDB("../storage/users.json")
+    users = db.table("users")
+
+    userToken = str(uuid.uuid4().hex)
+    userID = str(uuid.uuid4().hex)
+
+    # First check if user already exists
+    if users.search(tinydb.Query().userEmail == userEmail):
+        return False, None, None
+    else:
+        # If it doesn't, create a new entry
+        users.insert({"userID": userID, "userEmail": userEmail, "userName": userName, "userDisplayName": userDisplayName, "userInfo": "", "userInstructions": "", "token": [userToken]})
+        return True, userID, userToken
+
+def getUser(userID: str = None, userEmail: str = None, userName: str = None):
+    db = tinydb.TinyDB("../storage/users.json")
+    users = db.table("users")
+
+    if userID != None:
+        return users.search(tinydb.Query().userID == userID)
+    elif userEmail != None:
+        return users.search(tinydb.Query().userEmail == userEmail)
+    elif userName != None:
+        return users.search(tinydb.Query().userName == userName)
+    else:
+        return None
+
+def updateUser(userID: str, userEmail: str = None, userName: str = None, userDisplayName: str = None, userInfo: str = None, userInstructions: str = None):
+    db = tinydb.TinyDB("../storage/users.json")
+    users = db.table("users")
+
+    # First check if user already exists
+    if users.search(tinydb.Query().userID == userID):
+        # If it does, modify the existing entry
+        if userEmail != None:
+            users.update({"userEmail": userEmail}, tinydb.Query().userID == userID)
+        if userName != None:
+            users.update({"userName": userName}, tinydb.Query().userID == userID)
+        if userDisplayName != None:
+            users.update({"userDisplayName": userDisplayName}, tinydb.Query().userID == userID)
+        if userInfo != None:
+            users.update({"userInfo": userInfo}, tinydb.Query().userID == userID)
+        if userInstructions != None:
+            users.update({"userInstructions": userInstructions}, tinydb.Query().userID == userID)
+
+        return True
+    else:
+        return False
+
+def getUserByToken(token: str):
+    db = tinydb.TinyDB("../storage/users.json")
+    users = db.table("users")
+
+    # Tokens looks something like this: "tokens": ["7fc7256e795a456d917465c837ecd588"]
+    for user in users.all():
+        if token in user['tokens']:
+            return user
+
+def generateNewToken(userID: str) -> str:
+    db = tinydb.TinyDB("../storage/users.json")
+    users = db.table("users")
+
+    userToken = str(uuid.uuid4().hex)
+
+    # First check if user already exists
+    if users.search(tinydb.Query().userID == userID):
+        # If it does, modify the existing entry
+        users.update(tinydb.operations.add("token", userToken), tinydb.Query().userID == userID)
+        return userToken
+    else:
+        return None
--- src/main.py
+++ src/main.py
@@ -8,11 +8,13 @@
 # Imports
 # ------------------------------------------------------
 
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, request
 from flask_htmx import HTMX
 from markupsafe import escape
 
 import os
+
+from api.users import createUser, getUser, updateUser, getUserByToken
 
 # ------------------------------------------------------
 # Constants
@@ -30,13 +32,52 @@ htmx = HTMX(app)
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    # If method is GET, return the login page
+    print(request.method)
+    if request.method == 'GET':
+        return render_template('login.html')
+    # If method is POST, check the login credentials
+    elif request.method == 'POST':
+        tokenInput = request.form['token']
+        # Check if the token is valid
+        try:
+            user = getUserByToken(tokenInput)
+            print(user)
+            userID = user['userID']
+            return f"200 {userID}"
+        except Exception as e:
+            print(e)
+            return "401"
 
-@app.route('/sign-up')
+@app.route('/sign-up', methods=['GET', 'POST'])
 def signUp():
+    # If method is GET, return the sign-up page
+    if request.method == 'GET':
+        return render_template('sign-up.html')
+    # If method is POST, create the user
+    elif request.method == 'POST':
+        userEmail = request.form['email']
+        userName = request.form['name']
+        userDisplayName = request.form['displayName']
+        # Create the user
+        success, userID, token = createUser(userEmail, userName, userDisplayName)
+        # If the user was created, return the user's token
+        if success:
+            return f"200 {userID} {token}"
+        # If the user was not created, return an error
+        else:
+            return "401"
     return render_template('sign-up.html')
+
+@app.route('/notes/<userid>')
+def notes(userid):
+    return render_template('notes.html')
+
+@app.route('/settings/<userid>')
+def settings(userid):
+    return render_template('settings.html')
 
 # ------------------------------------------------------
 # Main - API
