57cecf7658fffdbf57c8517461e482c448dc63ef
Hannah <github@rennersh.eu>
2024-02-24 12:27:10 +0100

    Start of ui

--- backend/src/ai-tooling/llm.py
+++ backend/src/ai-tooling/llm.py
@@ -2,3 +2,59 @@
 ### File: llm.py
 ### Authors: Hannah Renners
 ### ----------------------------------------------------
+
+# ------------------------------------------------------
+# Imports
+# ------------------------------------------------------
+
+import tinydb
+
+# ------------------------------------------------------
+# LLM Storage Functions
+# ------------------------------------------------------
+
+def updateLLM(modelName: str, humanName: str, functionCalling: bool, toolCalling: bool, imageInput: bool, maxTokens: int, maxNewTokens: int, apiKey: str):
+    db = tinydb.TinyDB("../storage/models.json")
+    models = db.table("models")
+
+    # First check if model already exists
+    if models.search(tinydb.Query().modelName == modelName):
+        # If it does, modify the existing entry
+        models.update({"humanName": humanName, "functionCalling": functionCalling, "toolCalling": toolCalling, "imageInput": imageInput, "maxTokens": maxTokens, "maxNewTokens": maxNewTokens, "apiKey": apiKey}, tinydb.Query().modelName == modelName)
+    else:
+        # If it doesn't, create a new entry
+        models.insert({"modelName": modelName, "humanName": humanName, "functionCalling": functionCalling, "toolCalling": toolCalling, "imageInput": imageInput, "maxTokens": maxTokens, "maxNewTokens": maxNewTokens, "apiKey": apiKey})
+        
+    return True
+
+def getLLM(modelName: str, humanName: str = None):
+    db = tinydb.TinyDB("../storage/models.json")
+    models = db.table("models")
+
+    if humanName != None:
+        return models.search(tinydb.Query().humanName == humanName)
+    else:
+        return models.search(tinydb.Query().modelName == modelName)
+
+def deleteLLM(modelName: str):
+    db = tinydb.TinyDB("../storage/models.json")
+    models = db.table("models")
+
+    models.remove(tinydb.Query().modelName == modelName)
+
+    return True
+
+def listLLMs():
+    db = tinydb.TinyDB("../storage/models.json")
+    models = db.table("models")
+
+    return models.all()
+
+# ------------------------------------------------------
+# LLM Functions
+# ------------------------------------------------------
+
+
+if __name__ == "__main__":
+    print(updateLLM("gpt-3", "GPT-3", True, True, True, 2048, 100, "1234"))
+    print(getLLM("gpt-3"))
--- backend/src/main.py
+++ backend/src/main.py
@@ -2,3 +2,57 @@
 ### File: main.py
 ### Authors: Hannah Renners
 ### ----------------------------------------------------
+
+
+# ------------------------------------------------------
+# Imports
+# ------------------------------------------------------
+
+from flask import Flask, render_template, send_file
+from flask_htmx import HTMX
+from markupsafe import escape
+
+import os
+
+# ------------------------------------------------------
+# Constants
+# ------------------------------------------------------
+
+
+# ------------------------------------------------------
+# Main - UI
+# ------------------------------------------------------
+
+app = Flask(__name__)
+htmx = HTMX(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/sign-up')
+def signUp():
+    return render_template('sign-up.html')
+
+# ------------------------------------------------------
+# Main - API
+# ------------------------------------------------------
+
+@app.route('/images/<filename>')
+def images(filename):
+    file = os.path.abspath(f'../storage/static/images/{escape(filename)}')
+    
+    # Check what file type it is
+    if filename.endswith(".jpg"):
+        return send_file(file, mimetype='image/jpg')
+    elif filename.endswith(".png"):
+        return send_file(file, mimetype='image/png')
+    elif filename.endswith(".ico"):
+        return send_file(file, mimetype='image/ico')
+
+if __name__ == '__main__':
+    app.run(debug=True)
